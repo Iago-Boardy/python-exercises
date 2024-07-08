3be8db48dcc6badffc2db52b245b412c82de5e8c
Iago Boardman Padilha <140920936+Iago-Boardy@users.noreply.github.com>
2024-07-08 01:28:30 -0300

    Add files via upload

---
+++ day-8-caesar-cypher/caesar-cypher.py
@@ -0,0 +1,34 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def caesar(start_text, shift_amount, cipher_direction):
+    end_text = ""
+    if cipher_direction == "decode":
+        shift_amount *= -1
+    shift_amount = shift_amount % 26  # Ajuste para valores de shift_amount maiores que 26
+    for char in start_text:
+        if char not in alphabet:
+            end_text += char
+        else:
+            position = alphabet.index(char)
+            new_position = (position + shift_amount) % 26
+            end_text += alphabet[new_position]
+    print(f"Here's the {cipher_direction}d result: {end_text}")
+
+# TODO-1: Import and print the logo from art.py when the program starts.
+from art import logo
+print(logo)
+
+# Loop principal do programa
+ask = True
+while ask:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+    print()
+    answer = input("Do you want to go again? Type 'yes' or 'no':\n").lower()
+
+    if answer == "no":
+        ask = False
+        print("Goodbye")
