232d795e9f29088389fd54c46953b6f6a6374fe5
Iago Boardy <iagogol2006@gmail.com>
2024-09-22 23:55:46 -0300

    Commiting changes

---
+++ day-7-hangman-game/exercise.py
@@ -0,0 +1,11 @@
+variable = range(1, 101)
+
+for i in variable:
+    if i % 5 == 0 and i % 3 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
--- day-8-caesar-cypher/caesar-cypher.py
+++ day-8-caesar-cypher/caesar-cypher.py
@@ -14,9 +14,6 @@ def caesar(start_text, shift_amount, cipher_direction):
             end_text += alphabet[new_position]
     print(f"Here's the {cipher_direction}d result: {end_text}")
 
-# TODO-1: Import and print the logo from art.py when the program starts.
-from art import logo
-print(logo)
 
 # Loop principal do programa
 ask = True
