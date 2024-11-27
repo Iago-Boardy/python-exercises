5240c4cbb8f58ba74c6e6630f22ac1e4f64fd33f
Iago Boardy <iagogol2006@gmail.com>
2024-11-27 11:53:34 -0300

    commit

---
+++ Random Exercises/lists-exercises.py
@@ -0,0 +1,12 @@
+
+def main():
+    lista = []
+    for i in range(1, 6):
+        valor = int(input(f"Digite o {i}° number: "))
+        lista.append(valor)
+    print(lista)
+
+
+
+if __name__ == "__main__":
+    main()
