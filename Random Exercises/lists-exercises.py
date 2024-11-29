2eeeffd8102c51990c0dae19fefdfc99f18e3443
Iago Boardman Padilha <iagogol2006@gmail.com>
2024-11-29 15:28:13 -0300

    commit this

--- Random Exercises/lists-exercises.py
+++ Random Exercises/lists-exercises.py
@@ -1,13 +1,30 @@
+gabarito = {
+    1: "D",
+    2: "A",
+    3: "C",
+    4: "B",
+    5: "A",
+    6: "D",
+    7: "C",
+    8: "C",
+    9: "A",
+    10: "B"
+}
 
-def main():
-    lista = []
-    for i in range(1, 6):
-        valor = int(input(f"Digite o {i}° number: "))
-        lista.append(valor)
-    lista.reverse()
-    print(lista)
+resposta_certa = []
+resposta_errada = []
+
+print("Temos 10 questões na prova, vamos verificar quantas questoes o aluno acertou.(A, B, C ou D)")
+
+for i in range(0, len(gabarito)):
+    resposta = input(f"Qual a resposta da {i+1}° questão: ").upper()
+    if resposta == gabarito[i+1]:
+        print("Resposta Correta - computando...")
+        resposta_certa.append(resposta)
+    else:
+        print("Resposta Errada - computando...")
+        resposta_errada.append(resposta)
 
 
-
-if __name__ == "__main__":
-    main()
+print("Respostas corretas", resposta_certa)
+print("Respostas erradas", resposta_errada)
